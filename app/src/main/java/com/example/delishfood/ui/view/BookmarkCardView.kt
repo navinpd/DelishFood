02e7bafb2d3426e89dcdd231123c51e01e7541d0
Navin <navin.prasad@dyson.com>
2022-12-20 22:26:56 +0800

    minor update

--- app/src/main/java/com/example/delishfood/ui/view/BookmarkCardView.kt
+++ app/src/main/java/com/example/delishfood/ui/view/BookmarkCardView.kt
@@ -3,11 +3,13 @@ package com.example.delishfood.ui.view
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,13 +24,12 @@ fun BookmarkCardView() {
             .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box {
+        Box(modifier = Modifier.padding(8.dp)) {
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher_background),
                 contentDescription = "Custom Image",
                 modifier = Modifier
                     .clip(RoundedCornerShape(15.dp))
-                    .fillMaxWidth(0.9f)
             )
         }
         Text(text = "45 Min")
